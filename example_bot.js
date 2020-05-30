071601dd03e6dbd6d0f4b7d084d65f20e4c2a6a2
Andrey Leshenko <andrey.leshenko@gmail.com>
2020-05-30 19:15:16 +0300

    Working graphical editor

---
+++ example_bot.js
@@ -0,0 +1,74 @@
+// Thrust types
+NEGATIVE_LARGE = -2;
+NEGATIVE_SMALL = -1;
+NONE = 0;
+POSITIVE_SMALL = 1;
+POSITIVE_LARGE = 2;
+
+//
+// This function is called on each simulation step.
+//
+// Input: The current position, rotation and time
+// Output: The control commands
+//
+
+var initialized = false;
+var lastX = undefined;
+var lastY = undefined;
+var lastZ = undefined;
+
+function calculateControl(
+	posX,		// - forward		+ backward
+	posY,		// - left			+ right
+	posZ,		// - down			+ up
+	yaw,		// - right			+ left
+	pitch,		// - down			+ up
+	roll,		// - CW				+ CCW
+	yawRate,	// speed with which the yaw is changing
+	pitchRate,	// speed with which the pitch is changing
+	rollRate,	// speed with which the roll is changing
+	time)		// in simulation steps, starts from 0
+{
+	var moveX		= NONE;
+	var moveY		= NONE;
+	var moveZ		= NONE;
+	var rotYaw		= NONE;
+	var rotPitch	= NONE;
+	var rotRoll		= NONE;
+
+	// Your control code
+	
+	function pid(t, dt, kp, kd) {
+		var res = kp * t + kd * dt;
+		if (res > 0)
+			return NEGATIVE_SMALL;
+		else
+			return POSITIVE_SMALL;
+	}
+
+	rotYaw = pid(yaw, yawRate, 1, -3);
+	rotPitch = pid(pitch, pitchRate, 1, -3);
+	rotRoll = pid(roll, rollRate, 1, -3);
+
+	var dx = posX - lastX;
+	var dy = posY - lastY;
+	var dz = posZ - lastZ;
+
+	lastX = posX;
+	lastY = posY;
+	lastZ = posZ;
+
+	if (!initialized) {
+		dx = dy = dz = 0;
+		initialized = true;
+	}
+
+	moveX = pid(posX + 0.2, dx, 1, 300);
+	moveY = pid(posY, dy, 1, 50);
+	moveZ = pid(posZ, dz, 1, 50);
+
+	// Return the control commands you want executed
+	return [moveX, moveY, moveZ, rotYaw, rotPitch, rotRoll];
+}
+
+
--- hook.js
+++ hook.js
@@ -13,18 +13,32 @@ window.__render = window.render;
 window.__timestep = 0;
 
 window.render = function() {
-	var control = calculateControl(
-		(camera.position.z - issObject.position.z),
-		(camera.position.x - issObject.position.x),
-		(camera.position.y - issObject.position.y),
-		fixedRotationY,
-		fixedRotationX,
-		fixedRotationZ,
-		.1 * rateRotationY,
-		.1 * rateRotationX,
-		.1 * rateRotationZ,
-		window.__timestep,
-	);
+	try {
+		var control = window.calculateControl(
+			(camera.position.z - issObject.position.z),
+			(camera.position.x - issObject.position.x),
+			(camera.position.y - issObject.position.y),
+			fixedRotationY,
+			fixedRotationX,
+			fixedRotationZ,
+			.1 * rateRotationY,
+			.1 * rateRotationX,
+			.1 * rateRotationZ,
+			window.__timestep,
+		);
+
+		if (!Array.isArray(control) || control.length != 6 || !control.every(function(x) { return [-2, -1, 0, 1, 2].indexOf(x) != -1; })) {
+			throw 'calculateControl returned value with wrong format! ' + control;
+		}
+
+		window.dispatchEvent(new CustomEvent('calculated_control', { detail: control }));
+	}
+	catch (error) {
+		console.error(error);
+		var control = [0, 0, 0, 0, 0, 0];
+
+		window.dispatchEvent(new CustomEvent('calculated_control', { detail: null }));
+	}
 
 	window.__timestep += 1;
 
@@ -35,11 +49,14 @@ window.render = function() {
 		[yawRight, yawLeft],
 		[pitchDown, pitchUp],
 		[rollRight, rollLeft],
-	]
+	];
 
 	var i;
 	for (i = 0; i < controlFunctions.length; i++) {
-		if (Math.abs(control[i] == 2)) {
+		var highThrustMode = rotationPulseSize == 1 || translationPulseSize == 0.005;
+		var highThrustRequired = Math.abs(control[i] == 2);
+		var switchMode = highThrustMode != highThrustRequired;
+		if (switchMode) {
 			updatePrecision('rotation');
 			updatePrecision('position');
 		}
@@ -49,8 +66,9 @@ window.render = function() {
 		else if (control[i] == 2 || control[i] == 1) {
 			controlFunctions[i][1]();
 		}
-		if (Math.abs(control[i]) == 2) {
-			resetPrecision();
+		if (switchMode) {
+			updatePrecision('rotation');
+			updatePrecision('position');
 		}
 	}
 
@@ -60,21 +78,22 @@ window.render = function() {
 
 //
 // Adding the code editor
+// (code taken from editor_window_mock.html)
 //
 
 // HTML
 
 var editorHTML = `
-<div class="bot-console">
+<div class="bot-editor-window">
 	<div class="bot-header">
-		Docking Bot!
-		<div class="bot-button-group">
-			<button id="bot-btn-deploy">Deploy code</button>
-			<button id="bot-btn-restart">Restart simulation</button>
-			<button id="bot-btn-minimize">Minimize Window</button>
-		</div>
+		ISS Docking Bot
+		<button id="bot-btn-restart">Update & Restart</button>
+		<button id="bot-btn-update">Update</button>
+		<button id="bot-btn-minimize">&#x23BD;</button>
+		<button id="bot-btn-maximize">&#x25AD;</button>
 	</div>
-	<pre id="bot-code-editor">hello world</pre>
+	<pre id="bot-code-editor"></pre>
+	<pre id="bot-output"></pre>
 </div>
 `;
 
@@ -85,29 +104,78 @@ document.body.appendChild(root);
 // CSS
 
 var editorCSS = `
-.bot-console {
-	width: 600px;
-	height: 400px;
+.bot-editor-window {
 	position: absolute;
-	top: 30px;
-	left: 30px;
+	display: flex;
+	flex-flow: column;
 	z-index: 100;
+
 	background-color: rgb(142, 142, 142);
 }
+
+/* Different window sizes */
+.bot-editor-window {
+	top: 30px;
+	left: 30px;
+	width: 600px;
+	height: 600px;
+}
+.bot-editor-window.maximized {
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+.bot-editor-window.minimized {
+	top: 30px;
+	left: 30px;
+	width: 600px;
+	height: auto;
+}
+
 .bot-header {
-	padding-left: 30px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	padding-left: 15px;
+	padding-right: 5px;
 	padding-top: 5px;
+	padding-bottom: 5px;
+
 	color: white;
 	font: 12px/20px Lato,Verdana,Arial,geneva,sans-serif;
-	font-size: 14px;
+	font-size: 14px;	
 }
-.bot-button-group {
-	display: inline-block;
-	padding-left: 30px;
+.bot-header button {
+	margin: 0 3px;
 }
+#bot-btn-restart {
+	margin-left: 30px;
+}
+#bot-btn-minimize {
+	margin-left: auto;
+}
+
 #bot-code-editor {
 	height: 100%;
-}`;
+	margin: 0;
+	flex-grow: 1;
+}
+.bit-editor-window.minimized > #bot-code-editor {
+	display: none;
+}
+#bot-output {
+	margin: 0;
+	padding: 5px 15px;
+	font-family: monospace;
+	color: white;
+	background-color: rgb(1, 63, 10);
+}
+#bot-output.bot-error{
+	background-color: rgb(189, 0, 2);
+}
+`;
 
 var style = document.createElement('style');
 style.appendChild(document.createTextNode(editorCSS))
@@ -121,24 +189,54 @@ script.onload = onEditorLoaded;
 document.body.appendChild(script); //or something of the likes
 
 //
-// All other editor logic
+// Editor logic (running the code, errors, minimize/maximize)
 //
 
 function onEditorLoaded() {
+	// Setting up the Ace editor
+
 	ace.config.set('basePath', 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.11');
 	var editor = ace.edit("bot-code-editor");
 	editor.session.setMode("ace/mode/javascript");
 	editor.setTheme("ace/theme/dracula");
 	editor.setValue(startBotCode, -1);
 	window.editor = editor;
+	// Prevent typing in the editor from activating simulator keyboard shortcuts
+	editor.container.addEventListener('keydown', function(e) { e.stopPropagation(); });
+	editor.container.addEventListener('keyup', function(e) { e.stopPropagation(); });
 
-	var buttonDeploy = document.getElementById('bot-btn-deploy');
+	// Minimize/maximize logic
+
+	var editorWindow = document.getElementsByClassName('bot-editor-window')[0];
+
+	document.getElementById('bot-btn-minimize').addEventListener('click', function() {
+		editorWindow.classList.toggle('minimized');
+		editorWindow.classList.remove('maximized');
+		editor.resize();
+	});
+	document.getElementById('bot-btn-maximize').addEventListener('click', function() {
+		editorWindow.classList.toggle('maximized');
+		editorWindow.classList.remove('minimized');
+		editor.resize();
+	});
+
+	// Deploy/restart logic
+
 	var buttonRestart = document.getElementById('bot-btn-restart');
-	var buttonMinimize = document.getElementById('bot-btn-minimize');
+	var buttonUpdate = document.getElementById('bot-btn-update');
 
-	function deployCode() {
+	var errorDuringDeployment = false;
+
+	function updateCode() {
 		// http://perfectionkills.com/global-eval-what-are-the-options/
-		window.eval(editor.getValue());
+		try {
+			window.eval(editor.getValue());
+			errorDuringDeployment = false;
+		}
+		catch (error) {
+			console.log(error);
+			errorDuringDeployment = true;
+		}
 	}
 
 	function resetPositionWithoutAnimation() {
@@ -151,19 +249,42 @@ function onEditorLoaded() {
 		camera.rotation.set(-20 * toRAD, -10 * toRAD, 15 * toRAD);
 	}
 
-	buttonDeploy.addEventListener('click', function() {
-		deployCode();
+	buttonUpdate.addEventListener('click', function() {
+		updateCode();
 	});
 
 	buttonRestart.addEventListener('click', function() {
-		deployCode();
+		updateCode();
 		resetPositionWithoutAnimation();
 		window.__timestep = 0;
 	});
 
-	buttonMinimize.addEventListener('click', function() {
-		console.log('Minimize!');
+	// Control output window
+	var output = document.getElementById('bot-output');
+
+	window.addEventListener('calculated_control', function(e) {
+		var control = e.detail;
+		if (errorDuringDeployment) {
+			output.innerText = 'Running the script failed! See JavaScript console for more details.';
+			output.classList.add('bot-error');
+		}
+		else if (control == null) {
+			output.innerText = 'Error while computing control! See JavaScript console for more details.';
+			output.classList.add('bot-error');
+		}
+		else {
+			res = '';
+			var i;
+			for (i = 0; i < control.length; i++) {
+				res += ' ' + control[i].toString().padStart(2);
+			}
+			output.innerText = 'Calculated control: [' + res + ' ]';
+			output.classList.remove('bot-error');
+		}
 	});
+
+	// Confirmation dialog on exit
+	window.onbeforeunload = function() { return "Did you save your stuff?"; }
 }
 
 startBotCode = `
@@ -182,16 +303,16 @@ POSITIVE_LARGE = 2;
 //
 
 function calculateControl(
-	posX,	// - forward		+ backward
-	posY,	// - left			+ right
-	posZ,	// - down			+ up
-	yaw,	// - right			+ left
-	pitch,	// - down			+ up
-	roll,	// - CW				+ CCW
-	yawRate,
-	pitchRate,
-	rollRate,
-	time)	// in simulation steps, starts from 0
+	posX,		// - forward		+ backward
+	posY,		// - left			+ right
+	posZ,		// - down			+ up
+	yaw,		// - right			+ left
+	pitch,		// - down			+ up
+	roll,		// - CW				+ CCW
+	yawRate,	// speed with which the yaw is changing
+	pitchRate,	// speed with which the pitch is changing
+	rollRate,	// speed with which the roll is changing
+	time)		// in simulation steps, starts from 0
 {
 	var moveX		= NONE;
 	var moveY		= NONE;
@@ -200,7 +321,7 @@ function calculateControl(
 	var rotPitch	= NONE;
 	var rotRoll		= NONE;
 
-	if (time === 0)
+	if (time < 3)
 		rotPitch = POSITIVE_LARGE;
 
 	// Return the control commands you want executed
